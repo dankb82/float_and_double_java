1e40e668ca5bd45a2d6e3f817654a09f7bf6891e
Daniel Buompane <danielbuompane@gmail.com>
2017-05-11 20:39:00 -0400

    Initial commit

---
+++ src/com/dankb/Main.java
@@ -0,0 +1,10 @@
+package com.dankb;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue =  5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5.25;
+    }
+}
