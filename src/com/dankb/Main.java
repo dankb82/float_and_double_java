9fbe206bd4f28b7e176e4ebefc1ab00628b599bc
Daniel Buompane <danielbuompane@gmail.com>
2017-05-11 20:50:53 -0400

    introduce division to show precision

--- src/com/dankb/Main.java
+++ src/com/dankb/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         int myIntValue =  5;
-        float myFloatValue = 5f;
-        double myDoubleValue = 5.25;
+        int testIntDivision = 5 / 2;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("The testIntDivision = " + testIntDivision);
+
+        //float has 7 decimal point precision
+        System.out.println("myFloatValue = " + myFloatValue);
+
+        //double has 16 decimal point precision
+        System.out.println("myDoubleValue = " + myDoubleValue);
     }
 }
